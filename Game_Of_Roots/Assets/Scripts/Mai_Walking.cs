0225dbcacca18b9b193f970459b3c1ecf74c7d47
DinosaurPower <lysym685@newschool.edu>
2023-02-04 14:38:29 -0500

    Now the code is working

--- Game_Of_Roots/Assets/Scripts/Mai_Walking.cs
+++ Game_Of_Roots/Assets/Scripts/Mai_Walking.cs
@@ -13,7 +13,7 @@ public class Mai_Walking : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+         characterPos = transform.position; //at first we fetch where the object is right now
     }
 
     // Update is called once per frame
@@ -22,10 +22,15 @@ public class Mai_Walking : MonoBehaviour
          //Vector3 mousePos = Input.mousePosition;
          if(Input.GetMouseButtonDown(0)){ //checking if the mouse is down
     characterPos = mainCamera.ScreenToWorldPoint(new Vector2(Input.mousePosition.x, Input.mousePosition.y)); //this function adapts mouse position from the screen to in-game screen
-gameObject.transform.position = Vector2.MoveTowards(transform.position, characterPos, speed*Time.deltaTime);
          }
+         gameObject.transform.position = Vector2.MoveTowards(transform.position, characterPos, speed*Time.deltaTime); //MoveTowards (from where, where to, speed) is a function tht drags objects somewhere
          
+         if (gameObject.trandform.position == charcaterPos){
+            Debug.Log("We are here")
+         } else { Debug.Log("Walking";)}
     }
+
+    //basically, the character is constantly moving toward the point, and the picked point is defined by mouse click
 
 
 }
