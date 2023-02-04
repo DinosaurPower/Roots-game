f2765e07b538e6e51902f234e58903c568803603
DinosaurPower <lysym685@newschool.edu>
2023-02-04 11:56:54 -0500

    Working on walking input

--- Game_Of_Roots/Assets/Scripts/Mai_Walking.cs
+++ Game_Of_Roots/Assets/Scripts/Mai_Walking.cs
@@ -1,9 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class Mai_Walking : MonoBehaviour
 {
+    public Vector3 characterPos;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,10 @@ public class Mai_Walking : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+         Vector3 mousePos = Input.mousePosition;
     }
+private void OnMouseClick(){
+    characterPos = mainCamera.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 500.0f));  
+}
+
 }
