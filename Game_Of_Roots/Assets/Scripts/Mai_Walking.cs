417b01745e7ece5b5922a47c96652ee5bcf99387
DinosaurPower <lysym685@newschool.edu>
2023-02-04 14:17:23 -0500

    Made code work

--- Game_Of_Roots/Assets/Scripts/Mai_Walking.cs
+++ Game_Of_Roots/Assets/Scripts/Mai_Walking.cs
@@ -19,13 +19,18 @@ public class Mai_Walking : MonoBehaviour
     void Update()
     {
          Vector3 mousePos = Input.mousePosition;
+         if(Input.GetMouseButtonDown(0)){
+            Debug.Log("calculating");
+    characterPos = mainCamera.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 500.0f)); //this function adapts mouse position from the screen to in-game screen
+    Debug.Log("Clicked"+mousePos); 
+         }
          
     }
 
-
-private void OnMouseClick(){
-    characterPos = mainCamera.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 500.0f)); 
-    Debug.Log("Clicked"+mousePos); 
+//the point function
+private void OnMouseDown() {
+    
+    
 }
 
 }
