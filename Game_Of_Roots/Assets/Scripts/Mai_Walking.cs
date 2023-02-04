dfc72bfd47ff355b86953cd7c1d24b4cf0a940b0
DinosaurPower <lysym685@newschool.edu>
2023-02-04 12:01:11 -0500

    added test object and work on walking code

--- Game_Of_Roots/Assets/Scripts/Mai_Walking.cs
+++ Game_Of_Roots/Assets/Scripts/Mai_Walking.cs
@@ -7,6 +7,8 @@ using UnityEngine.UI;
 public class Mai_Walking : MonoBehaviour
 {
     public Vector3 characterPos;
+    private Vector3 mousePos;
+    public Camera mainCamera;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,9 +19,13 @@ public class Mai_Walking : MonoBehaviour
     void Update()
     {
          Vector3 mousePos = Input.mousePosition;
+         Debug.log(mousePos);
     }
+
+
 private void OnMouseClick(){
-    characterPos = mainCamera.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 500.0f));  
+    characterPos = mainCamera.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 500.0f)); 
+    Debug.Log("Clicked"+mousePos); 
 }
 
 }
