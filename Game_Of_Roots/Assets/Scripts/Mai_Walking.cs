5332f2a8c1fea2addff00632d1b8dfb36033dcd2
DinosaurPower <lysym685@newschool.edu>
2023-02-04 14:45:58 -0500

    Walking code is working!!

--- Game_Of_Roots/Assets/Scripts/Mai_Walking.cs
+++ Game_Of_Roots/Assets/Scripts/Mai_Walking.cs
@@ -8,6 +8,7 @@ public class Mai_Walking : MonoBehaviour
 {
     public Vector2 characterPos;
     private Vector2 mousePos;
+    private Vector2 whereToGo;
     public Camera mainCamera;
     public float speed;
     // Start is called before the first frame update
@@ -21,13 +22,14 @@ public class Mai_Walking : MonoBehaviour
     {
          //Vector3 mousePos = Input.mousePosition;
          if(Input.GetMouseButtonDown(0)){ //checking if the mouse is down
-    characterPos = mainCamera.ScreenToWorldPoint(new Vector2(Input.mousePosition.x, Input.mousePosition.y)); //this function adapts mouse position from the screen to in-game screen
+    characterPos = mainCamera.ScreenToWorldPoint(new Vector2(Input.mousePosition.x, transform.position.y)); //this function adapts mouse position from the screen to in-game screen
+whereToGo = new Vector2(characterPos.x, transform.position.y); //a crutch to keep character y and mouse input x
          }
-         gameObject.transform.position = Vector2.MoveTowards(transform.position, characterPos, speed*Time.deltaTime); //MoveTowards (from where, where to, speed) is a function tht drags objects somewhere
+         gameObject.transform.position = Vector2.MoveTowards(transform.position, whereToGo, speed*Time.deltaTime); //MoveTowards (from where, where to, speed) is a function tht drags objects somewhere
          
-         if (gameObject.trandform.position == charcaterPos){
-            Debug.Log("We are here")
-         } else { Debug.Log("Walking";)}
+         if (gameObject.transform.position.x == characterPos.x){
+            Debug.Log("We are here"); } else { Debug.Log("Walking");}
+
     }
 
     //basically, the character is constantly moving toward the point, and the picked point is defined by mouse click
